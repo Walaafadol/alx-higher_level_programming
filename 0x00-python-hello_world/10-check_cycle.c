28efb9adecace63dcfc55663b2cf8b3b74699458
Walaafadol <walaafadul7@gmail.com>
2023-10-30 15:46:54 -0700

    ll

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include  <stdlib.h>
+#include <stdio.h>
+/**
+*check_cycle - print anam
+*@list:argumeny
+*Return: void
+*/
+int check_cycle(listint_t *list)
+{
+listint_t "slow = list, *fast = list;
+while(fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+return (1);
+}
+return (0);
+}
