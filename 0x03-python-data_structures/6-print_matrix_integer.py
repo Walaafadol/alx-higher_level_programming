68f870188af87939f91c3ba3b3b35d77ea7c2314
Walaafadol <walaafadul7@gmail.com>
2023-11-05 11:48:13 -0800

    ddd

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        return none
+        return None
     for submatrix in matrix:
         if len(submatrix) == 0:
             print()
