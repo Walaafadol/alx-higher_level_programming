fdbcc175d3a237d934d771fa6b0a508e84cb48aa
Walaafadol <walaafadul7@gmail.com>
2023-11-08 11:50:50 -0800

    ggggg

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,8 +5,7 @@ def best_score(a_dictionary):
     maxvalue = 0
     maxkey = None
     for key, value in a_dictionary.items():
-        if value  >  maxvalue:
+        if value > maxvalue:
             maxvalue = value
             maxkey = key
     return maxkey
-
