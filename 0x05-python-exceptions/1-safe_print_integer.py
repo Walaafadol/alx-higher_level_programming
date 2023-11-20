bf54724f39ac9583d843d3b8f2ffbf40b99e2733
Walaafadol <walaafadul7@gmail.com>
2023-11-20 08:17:48 -0800

    ll

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(vaue))
         return True
-    except (ValueError, TypeError):
-        return false
+    except (TypeError, ValueError):
+        return False
