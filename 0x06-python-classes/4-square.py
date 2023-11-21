45e86d7f2c8814946610e717e749ccaa7ad7a272
Walaafadol <walaafadul7@gmail.com>
2023-11-21 11:40:32 -0800

    ll

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -30,10 +30,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
     def area(self):
-        """Initialize area of  Square.
+        """Initialize area of Square.
 
 
         Returns:
         area of square.
         """
-        return self.__size ** 2
+        return self.__size **2
