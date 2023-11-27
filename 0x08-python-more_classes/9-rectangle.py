6d115dcf296d19dc61988c21fa5b8405b3f3cd7c
Walaafadol <walaafadul7@gmail.com>
2023-11-27 13:20:22 -0800

    ll

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -16,19 +16,19 @@ class Rectangle:
 
     @property
     def width(self):
-        """ width
+        """width
         """
         return self.__width
 
     @property
     def height(self):
-        """ height
+        """height
         """
         return self.__height
 
     @width.setter
     def width(self, value):
-        """ width setter
+        """width setter
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -38,13 +38,25 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """ height setter
+        """height setter
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return Rectangle(size, size)
 
     def area(self):
         """ returns rectangle area"""
