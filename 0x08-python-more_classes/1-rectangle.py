41a3aa0645f64554809fb875fdd0ce50dfb7b4e3
Walaafadol <walaafadul7@gmail.com>
2023-11-27 09:44:42 -0800

    ll

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,8 +3,8 @@
 class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialises the data"""
-        self.__width = width
         self.__height = height
+        self.__width = width
     @property
     def width(self):
         """getter for the width"""
