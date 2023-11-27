962a44f77c873bd05aec740868f0490f392ab393
Walaafadol <walaafadul7@gmail.com>
2023-11-27 12:55:30 -0800

    ll

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -11,6 +11,8 @@ class Rectangle:
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+
+
     @property
     def width(self):
         """ width
@@ -69,5 +71,6 @@ class Rectangle:
     def __del__(self):
         """Print the message when an instance of Rectangle is deleted
         """
+
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
