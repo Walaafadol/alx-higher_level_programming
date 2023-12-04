5f9fc4d1bf2e927350beadd73734ebe9686bfa09
Walaafadol <walaafadul7@gmail.com>
2023-12-04 13:09:30 -0800

    ll

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Rectangle module"""
+def lookup(obj):
+    """ ookup object
+    arg:
+        obj (object) : the argument
+    Returns:
+        list : lists
+    """
+    return dir(obj)
