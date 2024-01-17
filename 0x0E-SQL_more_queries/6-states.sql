038a4392bf1f64fd1e47bf62df33fb69b1a6d6ce
Walaafadol <walaafadul7@gmail.com>
2024-01-17 12:58:41 -0800

    l

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- Write a script that creates the database hbtn_0d_usa and the table states
 CREATE database IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NOT NULL, UNIQUE(ID),
-AUTO_INCREMENT(ID), PRIMARY KEY(ID), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
